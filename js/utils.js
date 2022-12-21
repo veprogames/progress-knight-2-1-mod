d47da518e90e33716c9e69d9b882b3283f5a6c1f
veprogames <veprogames.gamedev@gmail.com>
2022-12-21 14:18:38 +0100

    add more currencies

--- js/utils.js
+++ js/utils.js
@@ -34,37 +34,38 @@ function format(number, decimals = 1) {
 }
 
 function formatCoins(coins, element) {
-    const platina = Math.floor(coins / 1e6)
-    const gold = Math.floor((coins - platina * 1e6) / 1e4)
-    const silver = Math.floor((coins - platina * 1e6 - gold * 1e4) / 100)
-    const copper = Math.floor(coins - platina * 1e6 - gold * 1e4 - silver * 100)
-
-    const money = {
-        "p": { "color": "#79b9c7", "value": platina },
-        "g": { "color": "#E5C100", "value": gold },
-        "s": { "color": "#a8a8a8", "value": silver },
-        "c": { "color": "#a15c2f", "value": copper },
-    }
-
-    /*const money2 = [
+    const money2 = [
+        { "name": "✹", "color": "#ffffcc", "value": 1e30 },
+        { "name": "∰", "color": "#ff0083", "value": 1e26 },
+        { "name": "𐙋", "color": "#27b897", "value": 1e23 },
+        { "name": "𐅍", "color": "#cd72ff", "value": 1e20 },
+        { "name": "Δ", "color": "#f5c211", "value": 1e17 },
+        { "name": "d", "color": "#ffffff", "value": 1e14 },
+        { "name": "r", "color": "#ed333b", "value": 1e12 },
+        { "name": "S", "color": "#6666ff", "value": 1e10 },
+        { "name": "e", "color": "#2ec27e", "value": 1e8 },
         { "name": "p", "color": "#79b9c7", "value": 1e6 },
         { "name": "g", "color": "#E5C100", "value": 10000 },
         { "name": "s", "color": "#a8a8a8", "value": 100 },
         { "name": "c", "color": "#a15c2f", "value": 1 },
-    ];*/
+    ];
 
-    let i = 0
-    for (const key in money) {
-        if(i >= 2) break;
-        const m = money[key];
-        if ((m.value > 0 || key == "c")) {
-            element.children[i].textContent = format(m.value, m.value < 1000 ? 0 : 1) + key
-            element.children[i].style.color = m.color
-            i++
+
+    for(const c of element.children){
+        c.textContent = "";
+    }
+    let c = 0
+    for (let i = 0; i < money2.length; i++) {
+        const m = money2[i];
+        const prev = money2[i - 1];
+        const diff = prev ? prev.value / m.value : Infinity;
+        const amount = Math.floor(coins / m.value) % diff;
+        if ((amount > 0 || m.name == "c")) {
+            element.children[c].textContent = format(amount, amount < 1000 ? 0 : 1) + m.name
+            element.children[c].style.color = m.color
+            c++
         }
-        else {
-            element.children[i].textContent = ""            
-        }
+        if(c >= 2 || amount >= 100) break;
     }
 }
 
